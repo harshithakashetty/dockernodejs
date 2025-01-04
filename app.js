9b5d4985fd8e5f3f49268c118209f3652101f72d
harshithakashetty <harshithakashetty@gmail.com>
2025-01-04 14:15:18 +0530

    file is committed

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const express = require('express');
- const app = express(); 
-app.get('/',(req,res) => { 
-res.send("Docker Nodejs Example"); 
-}); 
-app.listen(3111,() => {
-console.log("Server is running on port 3111") 
-}); 
+console.log("hello world"); 
 
 
